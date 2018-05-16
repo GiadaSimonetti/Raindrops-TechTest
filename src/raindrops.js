40ee459d7579426c71bc23f3293dbe2048cba0ab
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-05-16 15:42:01 +0100

    Refactor the plingPlangPlong function

--- src/raindrops.js
+++ src/raindrops.js
@@ -1,22 +1,10 @@
 class Raindrops {
 
   plingPlangPlong(n){
-    if ( n % 3 === 0 && n % 5 === 0 && n % 7 === 0 ) {
-      return 'PlingPlangPlong';
-    } else if ( n % 3 === 0 && n % 5 === 0 ) {
-      return 'PlingPlang';
-    } else if ( n % 3 === 0 && n % 7 === 0 ) {
-      return 'PlingPlong';
-    } else if ( n % 5 === 0 && n % 7 === 0 ) {
-      return 'PlangPlong';
-    } else if ( n % 3 === 0 ) {
-      return 'Pling';
-    } else if ( n % 5 === 0 ) {
-      return 'Plang';
-    } else if ( n % 7 === 0 ) {
-      return 'Plong';
-    } else {
-      return n;
-    }
+    let string = '';
+    if( n % 3 === 0 ) string += 'Pling';
+    if( n % 5 === 0 ) string += 'Plang';
+    if( n % 7 === 0 ) string += 'Plong';
+    return string || n;
   }
 }
