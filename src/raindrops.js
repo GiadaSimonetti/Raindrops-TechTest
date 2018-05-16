469287eb27a4103dd29a3eea4e8bad48d0440de4
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-05-16 21:16:27 +0100

    Refactor code in raindrops.js file

--- src/raindrops.js
+++ src/raindrops.js
@@ -2,9 +2,9 @@ class Raindrops {
 
   plingPlangPlong(n){
     let string = '';
-    if (!( n % 3 )) string += 'Pling';
-    if (!( n % 5 )) string += 'Plang';
-    if (!( n % 7 )) string += 'Plong';
+    if( n % 3 === 0 ) string += 'Pling';
+    if( n % 5 === 0 ) string += 'Plang';
+    if( n % 7 === 0 ) string += 'Plong';
     return string || n;
   }
 }
