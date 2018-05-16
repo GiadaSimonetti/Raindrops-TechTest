459efd3e046a759c384f09aa44aa1da544fa6231
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-05-16 15:13:59 +0100

    Create the third test that checks if the function returns  when  the number has a factor of 7

--- tests/raindropsTest.js
+++ tests/raindropsTest.js
@@ -12,4 +12,8 @@ describe('Raindrops features test', () => {
   it('returns `Plang` if the number has a factor of 5', () => {
     expect(raindrops.plingPlangPlong(5)).toEqual('Plang');
   })
+
+  it('returns `Plong` if the number has a factor of 7', () => {
+    expect(raindrops.plingPlangPlong(7)).toEqual('Plong');
+  })
 });
