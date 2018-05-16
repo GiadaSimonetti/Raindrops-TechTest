a2808e6ae9736fdf308bb441130d8d98a1f35348
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-05-16 15:13:07 +0100

    Pass the second test

--- src/raindrops.js
+++ src/raindrops.js
@@ -2,7 +2,9 @@ class Raindrops {
 
   plingPlangPlong(n){
     if ( n % 3 === 0 ) {
-      return 'Pling'
+      return 'Pling';
+    } else if ( n % 5 === 0 ) {
+      return 'Plang';
     }
   }
 }
